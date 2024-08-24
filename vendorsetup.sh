f400d900cc1eb77073b944569839abe975552de8
cranckv3 <Ankitshresthaas5@gmail.com>
2024-08-24 13:22:10 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,1 @@
-git clone https://android.googlesource.com/platform/external/gflags   external/gflags
-
-# OFOX
-
-export OF_FLASHLIGHT_ENABLE=0
-export FOX_VERSION="R11.1"
-export FOX_BUILD_TYPE=Unofficial
-export OF_MAINTAINER="Ninja"
+# git clone https://android.googlesource.com/platform/external/gflags   external/gflags
